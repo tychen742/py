c75e95bcc1c00ffe845d58a2d91938fbc2b20230
Tsangyao Chen <tychen742@gmail.com>
2026-02-28 23:11:21 -0600

    committed @ 2026-02-28-231121-0600 by tychen@Mac.fidelitycommunications.com

--- shared/download.py
+++ shared/download.py
@@ -1,7 +1,7 @@
 from os.path import basename, exists
 
-def download(url):
-    filename = basename(url)
+def download(url, dest=None):
+    filename = dest if dest is not None else basename(url)
     if not exists(filename):
         from urllib.request import urlretrieve
 
