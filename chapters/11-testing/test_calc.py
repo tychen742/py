92c5748a8c31bac618ec425bdcd42d6580289596
Tsangyao Chen <tychen742@gmail.com>
2026-04-21 13:07:02 -0500

    committed @ 2026-04-21-130702-0500 by tcn85@rtsjpl774kw20.managed.mst.edu

--- chapters/11-testing/test_calc.py
+++ chapters/11-testing/test_calc.py
@@ -1,8 +1,13 @@
-def test_add_positive():
-    assert add(2, 3) == 5
+# test_calc.py
+from calc import add, divide
 
-def test_add_negative():
-    assert add(-1, -1) == -2
+def test_add():
+    assert add(1, 1) == 2
 
-def test_subtract():
-    assert subtract(10, 4) == 6
+def test_divide():
+    assert divide(10, 2) == 5.0
+
+def test_divide_by_zero():
+    import pytest
+    with pytest.raises(ZeroDivisionError):
+        divide(1, 0)
