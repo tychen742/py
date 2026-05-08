1a82f5682f94d798b8a02397a412c7d801a893a6
Tsangyao Chen <tychen742@gmail.com>
2026-05-08 16:28:03 -0500

    committed @ 2026-05-08-162803-0500 by tcn85@rtsjpl774kw20.managed.mst.edu

--- sync.sh
+++ sync.sh
@@ -1,1 +1,5 @@
-rsync -avz --delete ~/workspace/py/_build/html/ tychen@thinkpy.org:/var/www/py/
+#!/usr/bin/env bash
+set -euo pipefail
+
+rsync -aL --delete _html_extra/ _build/html/
+rsync -avz --delete _build/html/ tychen@thinkpy.org:/var/www/py/
