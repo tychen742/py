962231dbaef0ad332a08a8eab2c336b2448fe52d
Tsangyao Chen <tychen742@gmail.com>
2026-09-08 15:19:44 -0500

    committed @ 2026-09-08-151944-0500 by tcn85@rtsjpl774kw20.managed.mst.edu

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
-a = 10
-while a > 0:
-    print(a)
-    a -= 1
+# a = 10
+# while a > 0:
+#     print(a)
+#     a -= 1
+# else:
+#     print("Countdown complete")
+    
+for i in range(10, 0, -1):
+    print(i)
 else:
     print("Countdown complete")
     
+if 5 > 2:
+    print("5 is greater than 2")
